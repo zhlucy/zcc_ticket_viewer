5cc57b901d9a65422806c0636cfe025b212a5539
Lucy Lu <zhixinlu@berkeley.edu>
2021-11-24 15:04:05 -0800

    Fetch all pages with cursor pagination

--- src/ticket_viewer.py
+++ src/ticket_viewer.py
@@ -13,11 +13,12 @@ def main():
     token = config['USER']['api_token']
     encrypted = base64.b64encode(bytes('{email_addr}/token:{api_token}'.format(email_addr=email, api_token=token), 'utf-8')).decode('utf-8')
     
-    url = "https://{}.zendesk.com/api/v2/tickets.json".format(subdomain)
+    url = "https://{}.zendesk.com/api/v2/tickets.json?page[size]=100".format(subdomain)
     response = requests.get(url, headers={'Authorization': 'Basic {}'.format(encrypted)})
 
     if response.status_code == 200:
-        viewer = Viewer(response.json()["tickets"]);
+        tickets = getTickets(response, encrypted)
+        viewer = Viewer(tickets);
         viewer.menu()
     elif response.status_code == 401:
         print("Authentication failed.")
@@ -30,6 +31,15 @@ def main():
 def env_set():
     variables = ["SUBDOMAIN", "EMAIL", "TOKEN"]
     return all([var in os.environ.keys() for var in variables])
+
+def getTickets(response, encrypted):
+    json = response.json()
+    tickets = json["tickets"]
+    while json["meta"]["has_more"]:
+        response = requests.get(json["links"]["next"], headers={'Authorization': 'Basic {}'.format(encrypted)})
+        json = response.json()
+        tickets.extend(json["tickets"])
+    return tickets
 
 if __name__ == '__main__':
     main()
